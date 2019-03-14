29a86f3fa8b8b7956537ae0fbdbfda060e559fce
David Konsumer <konsumer@jetboystudio.com>
2019-03-13 17:03:43 -0700

    made withStdout easier to read

--- src/hoc.js
+++ src/hoc.js
@@ -2,4 +2,7 @@ import React from 'react'
 import { StdoutContext } from 'ink'
 
 // HOC to give your component stdout
-export const withStdout = (Component) => props => <StdoutContext.Consumer>{({ stdout }) => <Component stdout={stdout} {...props} />}</StdoutContext.Consumer>
+export const withStdout = (Component) => props => (
+  <StdoutContext.Consumer>{
+    ({ stdout }) => <Component stdout={stdout} {...props} />
+  }</StdoutContext.Consumer>)

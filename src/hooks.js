d2d8935af67e8371b1032abab2fa2925b4b5e46e
David Konsumer <konsumer@jetboystudio.com>
2019-03-13 17:00:26 -0700

    somplified hook even further

--- src/hooks.js
+++ src/hooks.js
@@ -3,12 +3,8 @@ import { useState, useEffect } from 'react'
 // simple demo-counter hook
 export const useCounter = () => {
   const [counter, setCounter] = useState(0)
-  let i
   useEffect(() => {
-    i = setInterval(() => {
-      setCounter(c => c + 1)
-    }, 500)
-
+    const i = setInterval(() => setCounter(c => c + 1), 500)
     return () => clearInterval(i)
   })
   return [counter, setCounter]
